84d8d8e56825b843834ac7c6450ea12daf77e06d
lzn202 <18858194107@163.com>
2018-02-07 21:51:19 +0800

    day4

---
+++ day4.py
@@ -0,0 +1,51 @@
+# 输入某年某月某日，判断这一天是这一年的第几天？
+
+year = int(input("now_year:"))
+month = int(input("now_month:"))
+day = int(input("now_day:"))
+p = "today is %d days in now year"
+
+if year % 4 != 0:
+    two_month = 28
+else:
+    two_month = 29
+
+
+if month == 1:
+    days = day
+    print( p %days)
+elif month == 2:
+    days = 31 + day
+    print( p %days)
+elif month == 3:
+    days = 31 + two_month + day
+    print(p %days)
+elif month == 4:
+    days = 31 + two_month + 31 + day
+    print(p %days)
+elif month == 5:
+    days = 31 + two_month + 31 + 30 + day
+    print(p %days)
+elif month == 6:
+    days = 31 + two_month + 31 + 30 + 31 + day
+    print(p %days)
+elif month == 7:
+    days = 31 + two_month + 31 + 30 + 31 + 30 + day
+    print=(p %days)
+elif month == 8:
+    days = 31 + two_month + 31 + 30 + 31 + 30 + 31 + day
+    print(p %days)
+elif month == 9:
+    days = 31 + two_month + 31 + 30 + 31 + 30 + 31 + 31 + day
+    print(p %days)
+elif month == 10:
+    days = 31 + two_month + 31 + 30 + 31 + 30 + 31 + 31 + 30 + day
+    print(p %days)
+elif month == 11:
+    days = 31 + two_month + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + day
+    print(p %days)
+elif month == 12:
+    days = 31 + two_month + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + day
+    print(p %days)
+
+
